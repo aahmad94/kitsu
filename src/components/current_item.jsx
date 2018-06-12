56e76d323303a483c614ff23cff518c7387aecc0
aahmad94 <aka5vm@virginia.edu>
2018-06-11 20:11:48 -0700

    add date and synopsis sections to current item page

--- src/components/current_item.jsx
+++ src/components/current_item.jsx
@@ -7,13 +7,16 @@ export default ({item}) => {
   const title = item.attributes.titles[titleType];
   const coverImage = item.attributes.coverImage;
   const imageUrl = coverImage ? coverImage.original : "";
+  let date = Date.parse(item.attributes.createdAt); 
+  date = new Date(date).toUTCString().split(",")[1].split(" ").slice(0, 4).join(" ");
 
+  const synopsis = item.attributes.synopsis;
   return (
     <div className="item">
-      <div className="item-title">
-        {title}
-      </div>
+      <div className="item-title">{title}</div>
       <img className="cover-image" src={imageUrl} alt=""/>
+      <div className="date">{date}</div>
+      <div className="synopsis">{synopsis}</div>
     </div>
   );
 };
