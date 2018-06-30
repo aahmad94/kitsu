0896b00aa98918a0ec730fbc3be53a5cfc32e67f
aahmad94 <aka5vm@virginia.edu>
2018-06-30 02:00:37 -0700

    add fire guy logo

--- src/components/main_feed.jsx
+++ src/components/main_feed.jsx
@@ -25,25 +25,17 @@ export default class MainFeed extends React.Component {
   render() {
     if (this.state.currentItemId) {
       const item = this.state.items.filter(el => el.id == this.state.currentItemId)[0];
-      return (
-        <div className="item-page">
-          <SearchBar
-            onSearchTermChange={(term) => this.handleSearch(term)} />
-          < CurrentItem 
-            item={item}
-          />
-        </div>
-      );
+      return <div className="item-page">
+          <img className="fire-guy" src="https://t0.rbxcdn.com/27dc74636039d97ed988db177ba3adbe" alt="" />
+          <SearchBar onSearchTermChange={term => this.handleSearch(term)} />
+          <CurrentItem item={item} />
+        </div>;
     } else {
-      return (
-        <div className="search-feed">
-          <SearchBar 
-              onSearchTermChange={(term) => this.handleSearch(term)} />
-          <List 
-            items={this.state.items}
-            onItemSelect={(currentItemId) => this.handleItemSelect(currentItemId)}/>
-        </div>
-      );
+      return <div className="search-feed">
+          <img className="fire-guy" src="https://t0.rbxcdn.com/27dc74636039d97ed988db177ba3adbe" alt="" />
+          <SearchBar onSearchTermChange={term => this.handleSearch(term)} />
+          <List items={this.state.items} onItemSelect={currentItemId => this.handleItemSelect(currentItemId)} />
+        </div>;
     }
   }
 }
